66639c04dbd558198ef0b2e933727a7ea29d7c40
Neha Patel <neha.btech.cs17@iiitranchi.ac.in>
2020-04-22 21:16:21 +0530

    changes

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const path = require('path')
-const hbs = require('hbs')
 const app = express()
 const port = process.env.PORT || 3000
 
 app.set('view engine','hbs');
 
 const publicDirPath = path.join(__dirname,'/public')
-console.log(publicDirPath)
+
 app.use(express.static(publicDirPath))
 
 app.get('',(req,res)=>{
